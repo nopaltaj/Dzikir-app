fa360b169816f4fbb6019a6d151ef8a81d0b939b
nopaltaj <tajdhiya.arkan@gmail.com>
2023-10-27 09:04:01 +0700

    Initial commit

---
+++ app/src/main/java/com/idn/doadzikir/UI/Detail/DetailArtikelActivity.kt
@@ -0,0 +1,50 @@
+package com.idn.doadzikir.UI.Detail
+
+import android.os.Build.VERSION.SDK_INT
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.idn.doadzikir.R
+import com.idn.doadzikir.databinding.ActivityDetailArtikelBinding
+import com.idn.doadzikir.model.Artikel
+
+class DetailArtikelActivity : AppCompatActivity() {
+    private var _binding : ActivityDetailArtikelBinding? = null
+    private val binding get() = _binding as ActivityDetailArtikelBinding
+
+    companion object {
+        const val DATA_TITLE = "title"
+        const val DATA_DESC = "data"
+        const val DATA_IMAGE = "image"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        title = "Artikel Islami"
+        super.onCreate(savedInstanceState)
+        _binding = ActivityDetailArtikelBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val data = when{
+            SDK_INT >= 33 -> intent.getParcelableExtra("data", Artikel::class.java)
+            else -> @Suppress("DEPRECATION") intent.getParcelableExtra("data") as? Artikel
+        }
+
+
+
+        binding.apply {
+            data?.imageArtikel?.let { imgDetail.setImageResource(it) }
+            tvDetailTitle.text = data?.titleArtikel
+            tvDetailDesc.text = data?.descArtikel
+
+        }
+
+        val dataTitle = intent.getStringExtra(DATA_TITLE)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        finish()
+        return super.onSupportNavigateUp()
+    }
+
+}
---
+++ app/src/main/java/com/idn/doadzikir/UI/SetiapSaatDzikirActivity.kt
@@ -0,0 +1,36 @@
+package com.idn.doadzikir.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.idn.doadzikir.R
+import com.idn.doadzikir.adapter.DoadanDzikirAdapter
+import com.idn.doadzikir.databinding.ActivitySetiapSaatDzikirBinding
+import com.idn.doadzikir.model.DataDoaDzikir
+
+class SetiapSaatDzikirActivity : AppCompatActivity() {
+
+    private var _binding : ActivitySetiapSaatDzikirBinding? = null
+    private val binding get() = _binding as ActivitySetiapSaatDzikirBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        _binding = ActivitySetiapSaatDzikirBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val mAdapter = DoadanDzikirAdapter()
+        mAdapter.setData(DataDoaDzikir.listDzikir)
+        binding.rvDzikirSetiapSaat.adapter = mAdapter
+        binding.rvDzikirSetiapSaat.layoutManager = LinearLayoutManager(this)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        finish()
+        return super.onSupportNavigateUp()
+    }
+
+}
